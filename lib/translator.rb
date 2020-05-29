f9322ae37d11b905824580edff92d7463c768c31
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 20:23:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,7 @@ require "yaml"
 def load_library (yml_file)
   # code goes here
   emoticon_hash = YAML.load_file(yml_file)
-  emoticon_hash.each do |english_meaning_hash, emoticon_array|
-    english_meaning_hash={english_emoticon:
+
   
   
 end
