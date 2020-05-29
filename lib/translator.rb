b00fe1a9a6521b4542874b5f1dc014eb581d03cb
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 20:22:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,13 +12,13 @@ end
 def get_japanese_emoticon (file, western_emoticon)
   # code goes here
   load_library(file)
-  english_meaning = emoticon_hash.key(western_emoticon)
-  english_meaning[japanese_emoticon]
+  emoticon_meaning = emoticon_hash.key(western_emoticon)
+  emoticon_meaning[japanese]
 end
 
 def get_english_meaning(file, japanese_emoticon)
   # code goes here
   load_library(file)
-  english_meaning = emoticon_hash.key(japanese_emoticon)
-  english_meaning
+  emoticon_meaning_meaning = emoticon_hash.key(japanese_emoticon)
+  emoticon_meaning
 end
